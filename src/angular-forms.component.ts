e08421900d4499cb40e1c3921330692dc8d573ed
Erick Amaral <erick@voxtecnologia.com.br>
2019-02-04 10:10:55 -0300

    [FIX] #000 - Correção ortografia na montagem da resposta

--- src/angular-forms.component.ts
+++ src/angular-forms.component.ts
@@ -311,7 +311,7 @@ export class AngularFormsComponent implements OnInit, OnChanges, AfterViewChecke
             questionario: questions[0].quizId,
             grupo: questions[0].groupId,
             pergunta: questions[0].questionId,
-            respostas: answersGroups[group.code]
+            resposta: answersGroups[group.code]
           };
         });
       }
